0d9d758af36789a6f3c0b99dee7ef2213d0dd2a3
cauematheus <cauematheus_23@hotmail.com>
2024-04-22 22:22:36 -0300

    Adicionar arquivos de projeto.

---
+++ VetoresCrescente/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] vetor = new int[5];
+int maior = 0, aux = 0 ;
+for (int i = 0; i < 5; i++)
+{
+    do
+    {
+        Console.WriteLine($"Digite o {i + 1}° valor: ");
+        vetor[i] = int.Parse(Console.ReadLine());
+    } while (vetor[i] < 0);
+}
+for (int i = 0;i < 5; i++)
+{
+    if (vetor[i] > maior)
+    {
+        maior = vetor[i];
+    }
+}
