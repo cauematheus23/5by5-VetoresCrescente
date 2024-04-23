0fd44a797b0a93d9fa72b5be0eceaced7d970f51
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-23 16:01:45 -0300

    até o momento

--- VetoresCrescente/Program.cs
+++ VetoresCrescente/Program.cs
@@ -1,14 +1,15 @@
-﻿int size = 10, aux = 0;
+﻿int size = 5, aux = 0;
 int[] vetor = new int[size];
 int[] vetor2 = new int[size];
+int[] vetor3 = new int[size];
 
 for (int i = 0; i < size;i++)
 {
-    vetor[i] = new Random().Next(0,100);
+    vetor[i] = new Random().Next(0,size);
     vetor2[i] = vetor[i];
 }
 for (int i = 0; i < size; i++)
-{
+
     for (int j = i + 1; j < size; j++)
     {
         if (vetor2[i] > vetor2[j])
@@ -16,8 +17,15 @@ for (int i = 0; i < size; i++)
             aux = vetor2[i];
             vetor2[i] = vetor2[j];
             vetor2[j] = aux;
-
         }
+    }
+
+for (int i = 0; i < size; i++)
+{
+
+    if (vetor2[i] == vetor[i + 1])
+    {
+        vetor[i + 1] = vetor[i + 2];
     }
 }
 for (int i = 0; i < size; i++)
@@ -27,6 +35,6 @@ for (int i = 0; i < size; i++)
 Console.WriteLine();
 for (int i = 0; i < size; i++)
 {
-   
+
     Console.Write(vetor2[i] + " ");
 }
