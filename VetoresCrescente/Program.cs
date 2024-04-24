07f09c2e148e7ee4ae2b3365a2f531664f9c88e2
cauematheus <cauematheus_23@hotmail.com>
2024-04-23 23:02:41 -0300

    Copiada branch

--- VetoresCrescente/Program.cs
+++ VetoresCrescente/Program.cs
@@ -1,15 +1,14 @@
-﻿int size = 5, aux = 0;
+﻿int size = 10, aux = 0;
 int[] vetor = new int[size];
 int[] vetor2 = new int[size];
-int[] vetor3 = new int[size];
 
-for (int i = 0; i < size;i++)
+for (int i = 0; i < size; i++)
 {
-    vetor[i] = new Random().Next(0,size);
+    vetor[i] = new Random().Next(0, 100);
     vetor2[i] = vetor[i];
 }
 for (int i = 0; i < size; i++)
-
+{
     for (int j = i + 1; j < size; j++)
     {
         if (vetor2[i] > vetor2[j])
@@ -17,24 +16,33 @@ for (int i = 0; i < size; i++)
             aux = vetor2[i];
             vetor2[i] = vetor2[j];
             vetor2[j] = aux;
+
         }
     }
-
-for (int i = 0; i < size; i++)
-{
-
-    if (vetor2[i] == vetor[i + 1])
-    {
-        vetor[i + 1] = vetor[i + 2];
-    }
 }
+Console.Write("Vetor simples");
+Console.WriteLine();
 for (int i = 0; i < size; i++)
 {
     Console.Write(vetor[i] + " ");
 }
 Console.WriteLine();
+Console.WriteLine("Vetor Ordenado");
 for (int i = 0; i < size; i++)
 {
 
     Console.Write(vetor2[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Vetor Ordenado sem repetição");
+for (int i = 0; i < size;)
+{
+    aux = vetor2[i];
+    Console.Write(aux + " ");
+
+
+    while (i < size && vetor2[i] == aux)
+    {
+        i++;
+    }
 }
