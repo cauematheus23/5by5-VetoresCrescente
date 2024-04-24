6cee2cb103d869ce94b7989f132c2e7f4d11cb10
cauematheus <cauematheus_23@hotmail.com>
2024-04-23 23:31:46 -0300

    FUNCIONOU

--- VetoresCrescente/Program.cs
+++ VetoresCrescente/Program.cs
@@ -7,9 +7,22 @@ int[] vetor_desordenado = new int[size];
 
 for (int i = 0; i < size; i++)
 {
-    vetor[i] = new Random().Next(-50, -1);
+    vetor[i] = new Random().Next(0, 20);
     vetor_ordenado[i] = vetor[i];
     vetor_desordenado[i] = vetor[i];
+}
+for (int i = 0; i < size; i++)
+{
+    for (int j = i + 1; j < size; j++)
+    {
+        if (vetor_ordenado[i] > vetor_ordenado[j])
+        {
+            aux = vetor_ordenado[i];
+            vetor_ordenado[i] = vetor_ordenado[j];
+            vetor_ordenado[j] = aux;
+
+        }
+    }
 }
 for (int i = size -1; i >= 0; i--)
 {
@@ -24,14 +37,14 @@ for (int i = size -1; i >= 0; i--)
         }
     }
 }
-Console.Write("Vetor simples");
+Console.Write("Vetor gerado");
 Console.WriteLine();
 for (int i = 0; i < size; i++)
 {
     Console.Write(vetor[i] + " ");
 }
 Console.WriteLine();
-Console.WriteLine("Vetor Ordenado");
+Console.WriteLine("Vetor Ordenado com repetição");
 for (int i = 0; i < size; i++)
 {
 
@@ -46,6 +59,26 @@ for (int i = 0; i < size;)
 
 
     while (i < size && vetor_ordenado[i] == aux)
+    {
+        i++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Vetor Decrescente com repetição");
+for (int i = 0; i < size; i++)
+{
+
+    Console.Write(vetor_desordenado[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Vetor Decrescente sem repetição");
+for (int i = 0; i < size;)
+{
+    aux = vetor_desordenado[i];
+    Console.Write(aux + " ");
+
+
+    while (i < size && vetor_desordenado[i] == aux)
     {
         i++;
     }
